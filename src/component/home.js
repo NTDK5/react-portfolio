7e5e3f950cd9c29c85cbe52a0ed908e6c67b14b1
nati2345 <99682074+nati2345@users.noreply.github.com>
2023-05-01 14:07:37 +0300

    Update home.js

--- src/component/home.js
+++ src/component/home.js
@@ -1,7 +1,7 @@
 import React, {useEffect} from "react";
 import { faAngleDown } from "@fortawesome/free-solid-svg-icons";
 import {Link} from "react-scroll";
-// import Typed from "typed.js";
+import Typed from "typed.js";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "./style/home.css";
 
@@ -9,38 +9,38 @@ const description = "web-developer/web designer."
 
 const  Home = ()=>{
 
-    // // Create reference to store the DOM element containing the animation
-	// const el = React.useRef(null);
-    // // Create reference to store the Typed instance itself
-    //   const typed = React.useRef(null);
+    // Create reference to store the DOM element containing the animation
+	const el = React.useRef(null);
+    // Create reference to store the Typed instance itself
+      const typed = React.useRef(null);
   
-    // useEffect(() => {
-    //   const options = {
-    //       strings: ['Hello, my name is Natan.'],
-    //     typeSpeed: 100,
-    //   };
+    useEffect(() => {
+      const options = {
+          strings: ['Hello, my name is Natan.'],
+        typeSpeed: 100,
+      };
       
-    //   // elRef refers to the <span> rendered below
-    //   typed.current = new Typed(el.current, options);
+      // elRef refers to the <span> rendered below
+      typed.current = new Typed(el.current, options);
       
-    //   return () => {
-    //     // Make sure to destroy Typed instance during cleanup
-    //     // to prevent memory leaks
-    //     typed.current.destroy();
-    //   }
-    // }, [])
+      return () => {
+        // Make sure to destroy Typed instance during cleanup
+        // to prevent memory leaks
+        typed.current.destroy();
+      }
+    }, [])
 
 
         return(
             <section className="home" id='home'>
                 <div className="hero">
                      <div className="hero-img"></div>
-                    {/* <div className="hero-text">
+                    <div className="hero-text">
                         <div  className="type">
                             <h1 ></h1>
                         </div>
                         <h3>{description}</h3>
-                    </div> */}
+                    </div>
                     {/* <Link to="about" smooth={true} duration={500}>
                         <div className="downicon">
                             <FontAwesomeIcon icon={faAngleDown} className={"fa-2x"} />
